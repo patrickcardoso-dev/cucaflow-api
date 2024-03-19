3724dcb48f4fb6eb865e5ad86c5d6fc2e24465f7
Patrick Cardoso <dev.patrickcardoso@gmail.com>
2024-03-18 23:57:03 -0300

    chore: use PRISMA_BINARY_TARGET to set prisma's binary targets

--- prismaCyclicSetup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This was necessary to handle Cyclic size limits. When native is included (necessary for windows) the limit is exceeded.
-sed -i 's/\"native\"\, //' prisma/schema.prisma
