17ca2f477b7a1c4c689c4fb1f128cba17ce06f49
Iran <iran_filho@yahoo.com.br>
2024-04-04 19:07:04 -0300

    add useGuards

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 
 @Controller('task')
 export class TaskController {
@@ -13,16 +14,19 @@ export class TaskController {
     return this.taskService.create(id_user, createTaskDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id_user')
   async findAll(@Param('id_user') id_user: string) {
     return this.taskService.findAll(id_user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() UpdateTaskDto: UpdateTaskDto) {
     return this.taskService.update(id, UpdateTaskDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id_user')
   async remove(@Param('id_user') id_user: string, @Param('id_user') v: string): Promise<void> {
     try {
